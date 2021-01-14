d3d88107dc9afd1b3ec67dcf1214fdb8659a2ff9
Karina <alvarez-webdev@gmail.com>
2021-01-13 23:24:26 -0500

    commit

---
+++ week-3/secure-profile-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  app.module.ts
+; Author: Professor Krasso
+; Date:   8 January 2021
+; Modified by: Karina Alvarez
+; Description: app module
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent,
+    SignInComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-3/secure-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,52 @@
+/*
+============================================
+; Title:  app.component.ts
+; Author: Professor Krasso
+; Date:   8 January 2021
+; Modified by: Karina Alvarez
+; Description: app component
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+//TypeScript Person class with different variables
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    '#TypeScript', '#2020', '#CodingWithAngular', '#ngOmaha'
+  ];
+
+  constructor (fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+
+  //The toString method that will output the content of the class
+  toString() {
+    console.log(`\n Full Name: ${this.fullName}\n Favorite Food: ${this.favoriteFood}\n Favorite Color:  ${this.favoriteColor}`);
+  }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  //variable myProfile will be a person datatype
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person ("Karina Alvarez", "Chilean Empanadas", "Purple");
+    this.myProfile.toString();
+  }
+
+  ngOnInit(): void {
+  }
+
+}

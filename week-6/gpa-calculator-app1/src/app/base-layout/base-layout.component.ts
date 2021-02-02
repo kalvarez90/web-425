b2e20f2adaa01ffdc6b1f4bed6d49911b4ab0fa5
Karina <alvarez-webdev@gmail.com>
2021-02-02 17:03:28 -0500

    commit

---
+++ week-6/gpa-calculator-app1/src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  base-layout.component.ts
+; Author: Professor Krasso
+; Date:   2 February 2021
+; Modified by: Karina Alvarez
+; Description: base layout file
+;===========================================
+*/
+
+//These are files being imported from external files
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.css']
+})
+
+// assignment name that will be embedded in the base-layout.component.html file
+export class BaseLayoutComponent implements OnInit {
+
+  assignment: string;
+
+  constructor() {
+    this.assignment = "Exercise 6.3 - Layout"
+   }
+
+  ngOnInit(): void {
+  }
+
+}
